0df275df5a41645d5f3cc4003aad866d3ebaf6d7
Alexander Kykc <mad.kykc@gmail.com>
2018-10-31 13:05:40 +0200

    Implement #4

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,54 @@ extern crate colored;
 
 use colored::*;
 
-pub fn search(highlight: bool, pattern: &str, target: &str, callback: &mut FnMut(&str)) {
-    let re = regex::Regex::new(pattern);
-    for entry in walkdir::WalkDir::new(target).into_iter().filter_map(|e| e.ok()) {
+#[allow(dead_code)]
+const EMPTY_STRING: &'static str = "";
+const DEFAULT_PATTERN: &'static str = ".*";
+const DEFAULT_TARGET: &'static str = ".";
+
+#[derive(Clone, Debug)]
+pub struct SearchArgs<'a> {
+    pub highlight: bool,
+    pattern: &'a str,
+    pub target: &'a str,
+    pub case_insensitive: bool
+}
+
+impl<'a> SearchArgs<'a> {
+    pub fn with_pattern(&self, pattern: &'a str) -> Self {
+        let mut result = self.clone();
+        result.pattern = pattern;
+
+        result
+    }
+
+    pub fn construct_pattern(&self) -> String {
+        if self.case_insensitive {
+            String::from("(?i)") + self.pattern
+        } else {
+            String::from(self.pattern)
+        }
+    }
+}
+
+impl<'a> Default for SearchArgs<'a> {
+    fn default() -> Self {
+        SearchArgs {
+            highlight: true,
+            pattern: DEFAULT_PATTERN,
+            target: DEFAULT_TARGET,
+            case_insensitive: false
+        }
+    }
+}
+
+pub fn search<'a>(args: &'a SearchArgs, callback: &mut FnMut(&str)) {
+    let re = regex::Regex::new(&args.construct_pattern());
+    for entry in walkdir::WalkDir::new(args.target).into_iter().filter_map(|e| e.ok()) {
         match &re {
             Ok(reg) => {
                 if reg.is_match(entry.path().to_str().unwrap_or("")) {
-                    if highlight {
+                    if args.highlight {
                         let after = reg.replace_all(
                             entry.path().to_str().unwrap_or(""), "$0".yellow().to_string().as_str());
                         callback(&after);
@@ -27,9 +68,10 @@ mod tests {
     #[test]
     fn test_on_self() {
         let mut results = Vec::new();
+        let mut args = super::SearchArgs::default().with_pattern("\\.rs$");
+        args.highlight = false;
 
-        super::search(false, "\\.rs$", ".",
-                      &mut |x| results.push(String::from(x)));
+        super::search(&args, &mut |x| results.push(String::from(x)));
 
         assert_eq!(results.len(), 2);
         // TODO: will fail on windows
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,10 @@ use std::path::Path;
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 const HISTORY_FILE: &'static str = ".aufind_history";
 
-fn query_worker(target: &str) {
+const ARG_CASE_INSENSITIVE: &'static str = "CASE_INSENSITIVE";
+const ARG_PATTERN: &'static str = "PATTERN";
+
+fn query_worker(args: &mut aufindlib::SearchArgs) {
     let mut rl = Editor::<()>::new();
     let history_file = Path::new(&dirs::home_dir()
         .expect("Cannot get location of home dir")).join(HISTORY_FILE);
@@ -22,7 +25,7 @@ fn query_worker(target: &str) {
 
     match read_line {
         Ok(line) => {
-            aufindlib::search(true,&line, target, &mut |x| println!("{}", x));
+            aufindlib::search(&args.with_pattern(&line), &mut |x| println!("{}", x));
             rl.add_history_entry(line.as_ref());
             rl.save_history(&history_file).expect("Failed to store history");
         },
@@ -33,6 +36,11 @@ fn query_worker(target: &str) {
 }
 
 fn main() {
+    let arg_case_insensitive = Arg::with_name(ARG_CASE_INSENSITIVE)
+        .help("Toggle case insensitive")
+        .required(false)
+        .short("i")
+        .long("case-insensitive");
 
     let matches = App::new("aufind")
         .version(VERSION)
@@ -40,21 +48,25 @@ fn main() {
         .about("Simple file search utility")
         .subcommand(SubCommand::with_name("find")
                     .about("parse from CLI")
-                    .arg(Arg::with_name("PATTERN")
+                    .arg(arg_case_insensitive.clone())
+                    .arg(Arg::with_name(ARG_PATTERN)
                          .help("Pattern to match")
                          .required(false)
                          .takes_value(true)
                          .index(1)))
         .subcommand(SubCommand::with_name("query")
-                    .about("read from stdout"))
+                    .about("read from stdout")
+                    .arg(arg_case_insensitive.clone()))
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("find") {
-        let pattern = matches.value_of("PATTERN").unwrap_or(".*");
-        let target = ".";
-        aufindlib::search(true, pattern, target, &mut |x| println!("{}", x));
-    } else if let Some(_) = matches.subcommand_matches("query") {
-        let target = ".";
-        query_worker(target);
+        let mut args = aufindlib::SearchArgs::default()
+            .with_pattern(matches.value_of(ARG_PATTERN).unwrap_or(".*"));
+        args.case_insensitive = matches.is_present(ARG_CASE_INSENSITIVE);
+        aufindlib::search(&args, &mut |x| println!("{}", x));
+    } else if let Some(matches) = matches.subcommand_matches("query") {
+        let mut args = aufindlib::SearchArgs::default();
+        args.case_insensitive = matches.is_present(ARG_CASE_INSENSITIVE);
+        query_worker(&mut args);
     }
 }

54bef2cf3882b469f306d391839546e56a342708
Alexander Kykc <mad.kykc@gmail.com>
2018-10-31 17:25:04 +0200

    Disable highlighting by default on Windows, refactor case sensitivity flag

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ const ARG_CASE_INSENSITIVE: &'static str = "CASE_INSENSITIVE";
 const ARG_PATTERN: &'static str = "PATTERN";
 const ARG_INCLUDE_DIRS: &'static str = "INCLUDE_DIRS";
 const ARG_INCLUDE_FILES: &'static str = "INCLUDE_FILES";
+const ARG_HIGHLIGHT_OUTPUT: &'static str = "HIGHLIGHT_OUTPUT";
 
 const STRINGS_CONSIDERED_FALSE: [&'static str; 3] = ["false", "0", ""];
 
@@ -53,19 +54,24 @@ fn query_worker(args: &aufindlib::SearchArgs) {
 
 fn args_from_matches<'a>(matches: &clap::ArgMatches) -> aufindlib::SearchArgs<'a> {
     let mut args = aufindlib::SearchArgs::default();
-    args.case_insensitive = matches.is_present(ARG_CASE_INSENSITIVE);
+    args.case_insensitive = to_bool(matches.value_of(ARG_CASE_INSENSITIVE).expect("Defaulted option should always present"));
     args.include_dirs = to_bool(matches.value_of(ARG_INCLUDE_DIRS).expect("Defaulted option should always present"));
     args.include_files = to_bool(matches.value_of(ARG_INCLUDE_FILES).expect("Defaulted option should always present"));
+    args.highlight = to_bool(matches.value_of(ARG_HIGHLIGHT_OUTPUT).expect("Defaulted option should always present"));
 
     args
 }
 
 fn main() {
-    let arg_case_insensitive = Arg::with_name(ARG_CASE_INSENSITIVE)
+    let highlight_default = if cfg!(windows) { false } else { true };
+
+    let arg_case_sensitive = Arg::with_name(ARG_CASE_INSENSITIVE)
         .help("Toggle case insensitive")
         .required(false)
-        .short("i")
-        .long("case-insensitive");
+        .takes_value(true)
+        .default_value("true")
+        .short("c")
+        .long("case-sensitive");
 
     let arg_include_dirs = Arg::with_name(ARG_INCLUDE_DIRS)
         .help("Include directories in search results")
@@ -83,15 +89,24 @@ fn main() {
         .short("f")
         .long("include-files");
 
+    let arg_highlight_output = Arg::with_name(ARG_HIGHLIGHT_OUTPUT)
+        .help("Highlight matches in output")
+        .required(false)
+        .takes_value(true)
+        .default_value(if highlight_default {"true"} else {"false"})
+        .short("h")
+        .long("highlight-output");
+
     let matches = App::new("aufind")
         .version(VERSION)
         .author("Alexander Prokopchuk <ya@tomatl.org>")
         .about("Simple file search utility")
         .subcommand(SubCommand::with_name("find")
                     .about("parse from CLI")
-                    .arg(arg_case_insensitive.clone())
+                    .arg(arg_case_sensitive.clone())
                     .arg(arg_include_dirs.clone())
                     .arg(arg_include_files.clone())
+                    .arg(arg_highlight_output.clone())
                     .arg(Arg::with_name(ARG_PATTERN)
                          .help("Pattern to match")
                          .required(false)
@@ -99,9 +114,10 @@ fn main() {
                          .index(1)))
         .subcommand(SubCommand::with_name("query")
                     .about("read from stdout")
-                    .arg(arg_case_insensitive.clone())
+                    .arg(arg_case_sensitive.clone())
                     .arg(arg_include_dirs.clone())
-                    .arg(arg_include_files.clone()))
+                    .arg(arg_include_files.clone())
+                    .arg(arg_highlight_output.clone()))
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("find") {
